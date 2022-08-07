4b74cbeb1257153081bba8ba6be3b63ca6400756
AN8788 <88386844+AN8788@users.noreply.github.com>
2022-08-07 19:58:22 +0530

    Update streamlit_app.py
    Fixed emoji error

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,6 @@ streamlit.title("First test code on streamlit - Aug 7/2022")
 streamlit.header("This is header code")
 streamlit.text("Used text function")
 
-🥣 🥗 🐔 🥑🍞
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
