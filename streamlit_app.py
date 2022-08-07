09d9374a3548c72e049da36749b415421f51b18a
AN8788 <88386844+AN8788@users.noreply.github.com>
2022-08-07 19:57:57 +0530

    Update streamlit_app.py
    Added emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,13 @@ streamlit.title("First test code on streamlit - Aug 7/2022")
 streamlit.header("This is header code")
 streamlit.text("Used text function")
 
+🥣 🥗 🐔 🥑🍞
+streamlit.header('Breakfast Menu')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+
+
