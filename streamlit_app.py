f12731f85d34216519570e95d6bd612a2c8dac4b
AN8788 <88386844+AN8788@users.noreply.github.com>
2022-08-07 19:53:49 +0530

    Update streamlit_app.py
    Fixed typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,6 @@
 import streamlit 
 streamlit.title("First test code on streamlit - Aug 7/2022")
 
-stremlit.header("This is header code")
-stremlit.text("Used text function")
+streamlit.header("This is header code")
+streamlit.text("Used text function")
 
